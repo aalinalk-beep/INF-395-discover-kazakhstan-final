e35dcd62bd46046b7550da2d4f224a6cf04b8f8c
Alina Amangeldieva <alinaamangeldieva@MacBook-Pro-Alina.local>
2026-05-03 07:07:30 +0500

    changed settings.py

--- discover-kaz-backend/discover_kaz_backend/settings.py
+++ discover-kaz-backend/discover_kaz_backend/settings.py
@@ -156,5 +156,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://your-vercel-url.vercel.app",
 ]
+
 CORS_ALLOW_CREDENTIALS = True
