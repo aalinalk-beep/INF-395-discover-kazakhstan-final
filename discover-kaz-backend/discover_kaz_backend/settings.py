c41957db4d0b6649297784db08f1518a1af62501
Alina Amangeldieva <alinaamangeldieva@MacBook-Pro-Alina.local>
2026-05-03 10:03:53 +0500

    Fix CORS for Vercel frontend

--- discover-kaz-backend/discover_kaz_backend/settings.py
+++ discover-kaz-backend/discover_kaz_backend/settings.py
@@ -151,5 +151,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://discover-kazakhstan-final.vercel.app",
     "https://inf-395-final-project-abty067zt-aalinalk-beeps-projects.vercel.app",
 ]
